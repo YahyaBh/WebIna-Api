c8f1054c5a6987b68a94d2c19e81a25eafc149c0
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-07-08 12:02:31 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
@@ -10,12 +12,25 @@ Route::get('/', function () {
     return Redirect::to('https://webinadigital.com');;
 });
 
+
+//Home page routes 
 Route::get('/testimonials', [HomeController::class, 'testimonials']);
-Route::post('/contact' , [HomeController::class, 'contactSend']);
-Route::post('/register/email' , [HomeController::class, 'registerEmail']);
+Route::post('/contact', [HomeController::class, 'contactSend']);
+Route::post('/register/email', [HomeController::class, 'registerEmail']);
+
+
+//User regsitration routes
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+
+
 
 
 
 Route::middleware('auth:sanctum')->group(function () {
     // Protected routes that require authentication
+
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 });
