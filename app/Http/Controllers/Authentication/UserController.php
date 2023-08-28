2010b99add5501dd4d379dd1455583c996dd71af
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-08-28 17:47:59 +0100

    Update UserController.php

--- app/Http/Controllers/Authentication/UserController.php
+++ app/Http/Controllers/Authentication/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Authentication;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -74,44 +75,19 @@ class UserController extends Controller
                     'errors' => $validateUser->errors()
                 ], 401);
             }
-            
+
             $user = User::where('email', $request->email)->first();
 
-            
-            $remember = $request->has('remember'); // Check if remember me checkbox is checked
-            // $rememberToken = $user->remember_token;
-
-
-            if (!Auth::guard()->viaRemember()) {
-                if (!Auth::attempt($request->only(['email', 'password']), $remember)) {
-                    return response()->json([
-                        'status' => false,
-                        'message' => 'Email & Password do not match our records.',
-                    ], 401);
-                }
-
-
-
-                return response()->json([
-                    'status' => true,
-                    'message' => 'User Logged In Successfully',
-                    'token' => $user->createToken("access-token")->plainTextToken,
-                    // 'remember_token' => $rememberToken,
-                    'user' => $user
-                ], 200);
-            } else {
-                return response()->json([
-                    'status' => true,
-                    'message' => 'User Logged In Successfully',
-                    'token' => $user->createToken("access-token")->plainTextToken,
-                    // 'remember_token' => $rememberToken,
-                    'user' => $user
-                ], 200);
-            }
-        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken("access-token")->plainTextToken,
+                'user' => $user
+            ], 200);
+        } catch (Exception $e) {
             return response()->json([
                 'status' => false,
-                'message' => $th->getMessage()
+                'message' => $e->getMessage()
             ], 500);
         }
     }
